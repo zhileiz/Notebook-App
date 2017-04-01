316d82531cdc86e7a7bf46fb9fbb75da2318b272
zhileiz <zhileiz@seas.upenn.edu>
2017-04-01 15:48:16 -0400

    Add Note Views

--- db/migrate/20170401190412_create_notes.rb
+++ db/migrate/20170401190412_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration[5.0]
   def change
     create_table :notes do |t|
       t.string :title
-      t.text :context
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20170401190412) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
-    t.text     "context"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
