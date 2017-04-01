0969d3814e5d34fe40be70db42c17954ee8a5a15
zhileiz <zhileiz@seas.upenn.edu>
2017-04-01 17:12:41 -0400

    Last Commit before Styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :notes
   get 'welcome/index'
+
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
